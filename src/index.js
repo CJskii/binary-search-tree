ba801a424053c9a3a8df4446bfbd604ee602e9a1
CJskii <czareczarek3@gmail.com>
2022-12-06 23:44:45 +0000

    remove, levelorder, inorder, preorder, postorder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Node from "./node.js";
+import prettyPrint from "./prettyPrint.js";
 
 class Tree {
   constructor(array) {
@@ -12,7 +13,6 @@ class Tree {
     const root = new Node(arr[mid]);
     root.left = this.buildTree(arr, start, mid - 1);
     root.right = this.buildTree(arr, mid + 1, end);
-    //console.log({ root, left: root.left, right: root.right });
     return root;
   }
 
@@ -29,15 +29,75 @@ class Tree {
   }
 
   remove(value, root = this.root) {
-    if (root === null) return root;
-    const node = this.find(value, root);
-    console.log(node);
-    // if node has one child
-    if (node.right === null) {
-    } else if (node.left === null) {
-    }
-    // if node has two childs
-    else {
+    if (root === null) return null;
+
+    let currentNode = this.root;
+    let parentNode = null;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value == value) {
+        // found a match
+        // Option 1: no right child
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        }
+        // Option 2: Right child with no left child
+        else if (currentNode.right.left === null) {
+          currentNode.right.left = currentNode.left;
+          if (parentNode === null) {
+            this.root = currentNode.right;
+          } else {
+            // if parent > current - make right child of the leaf parent
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            }
+
+            // if parent < current - make right child a right child of the parent
+            else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+        } // Option 3: right child has a left child
+        else {
+          // find right child's left most child
+          let leftMost = currentNode.right.left;
+          let leftMostParent = currentNode.right;
+          while (leftMost.left != null) {
+            leftMostParent = leftMost;
+            leftMost = leftMost.left;
+          }
+
+          // Parent's left subtree is now leftmost's right subtree
+          leftMostParent.left = leftMost.right;
+          leftMost.left = currentNode.left;
+          leftMost.right = currentNode.right;
+
+          if (parentNode === null) {
+            this.root = leftMost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftMost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftMost;
+            }
+          }
+        }
+        return true;
+      }
     }
   }
 
@@ -51,35 +111,76 @@ class Tree {
       return root;
     }
   }
+
+  levelOrder(arr = [], queue = [], root = this.root) {
+    if (root === null) return root;
+    // Visiting root node
+    arr.push(root.value);
+
+    // add left and right children to the queue
+    queue.push(root.left);
+    queue.push(root.right);
+
+    // move to the next level
+    while (queue.length) {
+      const level = queue[0];
+      queue.shift();
+      this.levelOrder(arr, queue, level);
+    }
+    return arr;
+  }
+
+  inorder(arr = [], root = this.root) {
+    if (root === null) return root;
+
+    // traverse left subtree
+    if (root.left) this.inorder(arr, root.left);
+
+    // visit node
+    arr.push(root.value);
+
+    // traverse right subtree
+    if (root.right) this.inorder(arr, root.right);
+
+    return arr;
+  }
+
+  preorder(arr = [], root = this.root) {
+    if (root === null) return root;
+
+    // visit node
+    arr.push(root.value);
+
+    // traverse left subtree
+    if (root.left) this.preorder(arr, root.left);
+
+    // traverse right subtree
+    if (root.right) this.preorder(arr, root.right);
+
+    return arr;
+  }
+
+  postorder(arr = [], root = this.root) {
+    if (root === null) return root;
+
+    // traverse left subtree
+    if (root.left) this.preorder(arr, root.left);
+
+    //traverse right subtree
+    if (root.right) this.preorder(arr, root.right);
+
+    // visit node
+    arr.push(root.value);
+
+    return arr;
+  }
 }
 
 const tree = new Tree([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-tree.insert("10");
-tree.remove("10");
-
-/*
-
-find(value, root = this.root) {
-    if (root === null) return root;
-    if (value < root.value) {
-      root.left = this.find(value, root.left);
-    } else if (value > root.value) {
-      root.right = this.find(value, root.right);
-    } else {
-      console.log("returning");
-      return root;
-    }
-  }
-
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
+tree.insert(10);
+//tree.remove("7");
 
 prettyPrint(tree.root);
-*/
+//console.log(tree.inorder());
+//console.log(tree.preorder());
+//console.log(tree.postorder());
